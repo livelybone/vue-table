a1c674b8e83b98a3d80271c201c97ecdc2dbef38
livelybone <2631541504@qq.com>
2019-08-03 19:05:19 +0800

    feat(test): Update

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
-import VueTable from '../src/components/Index.vue'
+import * as VueTable from '../src/index.js'
 
 describe('Index.vue', () => {
   it('Rendered', () => {
-    const wrapper = shallowMount(VueTable)
+    const wrapper = shallowMount(VueTable.TableExtend)
     expect(wrapper.find('div').exists()).to.equal(true)
   })
 })
